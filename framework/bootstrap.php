1656467d11e429010c5a510d7cd9a3d0becb48c2
jmahony <jalmahony@gmail.com>
2013-02-26 22:39:45 +0000

    Load actions.php last so all functions are present

--- framework/bootstrap.php
+++ framework/bootstrap.php
@@ -10,10 +10,9 @@
 $framework_libraries = array(
 
   '/framework/defaults.php',
-  '/framework/actions.php',
   '/framework/lib/wpalchemy/MetaBox.php',
+  '/framework/lib/NHP-Theme-Options-Framework/nhp-options.php',
   '/framework/lib/CSS-Generator-for-NHP-Framework/css_generator.php',
-  '/framework/lib/NHP-Theme-Options-Framework/nhp-options.php', // TODO: this needs to be moved into includes
   '/framework/lib/helpers.php',
   '/framework/lib/enqueue.php',
   '/framework/lib/persistence.php'
@@ -22,19 +21,26 @@ $framework_libraries = array(
 
 $custom_libraries = array(
 
+  '/includes/rep_enqueue.php',
   '/includes/rep_meta_boxes.php',
   '/includes/rep_custom_types.php',
-  '/includes/rep_enqueue.php',
   '/includes/rep_menus.php',
   '/includes/rep_shortcodes.php',
   '/includes/rep_sidebars.php',
-  '/includes/rep_widgets.php',
+  '/includes/widgets/rep_widgets.php',
   '/includes/rep_categories.php',
   '/includes/rep_custom_styles.php',
-  '/includes/rep_admin_options.php'
+  '/includes/rep_admin_options.php',
 
 );
 
-\Rep\Core\load_library( array_merge( $framework_libraries, $custom_libraries ) );
+/* We need to include the actions last */
+$actions = array(
+
+  '/framework/actions.php'
+
+);
+
+\Rep\Core\load_library( array_merge( $framework_libraries, $custom_libraries, $actions ) );
 
 ?>
