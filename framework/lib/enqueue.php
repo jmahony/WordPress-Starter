624833ec65d317c8697db8d53d7fcb0b8e72275b
jmahony <jalmahony@gmail.com>
2013-03-07 14:33:17 +0000

    Stop queing scripts

--- framework/lib/enqueue.php
+++ framework/lib/enqueue.php
@@ -2,38 +2,12 @@
 /**
  * @package WordPress
  * @subpackage Template
- * 
+ *
  * Helper functions
- * 
+ *
  */
 
-function enqueue_scripts() {
-
-	// Get rid of WordPress default jQuery and include our version
-	wp_deregister_script( 'jquery' );
-
-	// Include jQuery
-	wp_register_script(
-		'jquery',
-		CURRENT_THEME_URL . '/framework/js/jquery-1.8.3.min.js',
-		false,
-		'1.8.2',
-		true
-	);
-
-	wp_register_script(
-		'html5shiv',
-		CURRENT_THEME_URL . '/framework/js/html5shiv.js',
-		false,
-		'3.6.2',
-		true
-	);
-
-	wp_enqueue_script( 'html5shiv' );
-
-	wp_enqueue_script( 'jquery' );
-
-}
+function enqueue_scripts() {}
 
 function enqueue_styles() {}
 
